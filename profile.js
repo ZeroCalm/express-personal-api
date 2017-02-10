a8f7d87b9fd809ab77c76a1d54dc1a500dd839eb
zeroCalm <smith.timothyh@gmail.com>
2017-02-10 15:55:03 -0800

    index and seed created

---
+++ profile.js
@@ -0,0 +1,19 @@
+
+{
+
+  Name: Timothy "ZeroCalm" Smith,
+  githubLink: https://github.com/ZeroCalm,
+  githubProfileImage:  TO BE INSERTED,
+  personalSiteLink:  www.TimothyHSmith.com,
+  currentCity: Vallejo, CA,
+  pets: [
+    {
+      name: "Frank",
+      type: "Crazy Mutt",
+      breed: "Cha-weenie"
+    }
+  ]
+
+
+
+}
--- seed.js
+++ seed.js
@@ -13,3 +13,41 @@
 //   console.log("Created new campsite", campsite._id)
 //   process.exit(); // we're all done! Exit the program.
 // })
+
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// description of structure
+var GameSchema = new Schema({
+  genre: String,
+  title: String,
+  isCool: Boolean
+});
+
+// turn it into a cookie model (which we can CRUD)
+var Game = mongoose.model('Game', GameSchema);
+
+// NAME AND CONNECT TO DATABASE
+mongoose.connect('mongodb://localhost/express-personal-api')
+
+var game = [
+  {genre: "Real Time Strategy", title: "Command And Conquer", isCool: true},
+  {genre: "Side-Scroller", title: "This War of Mine", isCool: true},
+  {genre: "First Person Shooter", title: "Harry Poter", isCool: false},
+
+];
+
+
+// //// remove all the cookies
+// Cookie.remove({}, function(err){
+//   if(err){ console.log(err); }
+//   // console.log("removed all the cookies")
+
+  // create the cookies
+  Game.create(game, function(err, game){
+    if(err){ console.log(err); }
+    console.log("seeded the db with", game.length, "games");
+    console.log(game);
+    process.exit();
+  });

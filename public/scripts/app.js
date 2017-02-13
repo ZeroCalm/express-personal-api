59328494f1ac48805ff3718b8252d71fa655cf32
zeroCalm <smith.timothyh@gmail.com>
2017-02-12 19:02:08 -0800

    GET ALL button working fullstack to the DB

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,21 @@
 console.log("Sanity Check: JS is working!");
 
+var games = [];
+
+
 $(document).ready(function(){
 
-// your code
-
+  $.ajax({
+      method: 'GET',
+    //  url: '/api/games',
+      success: handleSuccess,
+      error: handleError
+    });
 });
+
+
+
+function handleSuccess(json) {
+  games = json;
+  render();
+}
